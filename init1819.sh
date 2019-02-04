ee802a43df239f7da822a7f488785e80f0e5b0d0
Budi Darmawan <vbudi@us.ibm.com>
2019-02-04 13:52:36 -0600

    split bluemix and local script

--- init1819.sh
+++ init1819.sh
@@ -1,17 +1,4 @@
 #!/bin/bash
-  echo "Logging in as $1 with the password $2"
   ibmcloud config --check-version=false
-  echo 1 | ibmcloud login -a https://api.ng.bluemix.net -u $1@us.ibm.com -p $2 -g default
   ibmcloud plugin install kubernetes-service
   ibmcloud plugin install container-registry
-  ibmcloud cr namespace-add "$1"usibmcom
-
-  ibmcloud ks cluster-create --name cloudcluster
-
-  cnt=0
-  while [ $cnt -lt 1 ]
-  do
-    cnt=`ibmcloud ks clusters | grep normal | wc -l`
-    sleep 30
-  done
-  echo "Cluster created - setup successful"
