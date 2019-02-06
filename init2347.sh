ebad1fa85fa0cba96daa2bec526dd07a245b086d
Budi Darmawan <vbudi@us.ibm.com>
2019-02-06 06:44:33 -0600

    final 2347

--- init2347.sh
+++ init2347.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo passw0rd | sudo -S apt-get update && sudo apt-get upgrade
-echo passw0rd | sudo -S apt-get install default-jdk
+echo passw0rd | sudo -S apt-get update
+echo passw0rd | sudo -S apt-get -y install default-jdk
 
-cat <<EOF >> /etc/profile
-export JAVA_HOME=/usr/lib/jvm/java-9-openjdk-amd64
-export PATH=$PATH:$JAVA_HOME/bin
-EOF
