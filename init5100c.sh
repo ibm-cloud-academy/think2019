96862e8ae6f06e6c9a6e905125226e638de770e2
Budi Darmawan <vbudi@us.ibm.com>
2019-02-08 12:58:36 -0600

    ok

--- init5100c.sh
+++ init5100c.sh
@@ -3,15 +3,6 @@
   ibmcloud config --check-version=false
   echo 1 | ibmcloud login -a https://api.ng.bluemix.net -u $1@us.ibm.com -p $2 -g default
 
-  ibmcloud ks cluster-create --name cloudcluster
-
-  cnt=0
-  while [ $cnt -lt 1 ]
-  do
-    cnt=`ibmcloud ks clusters | grep normal | wc -l`
-    sleep 30
-  done
-
   `ibmcloud ks cluster-config cloudcluster | grep KUBECONFIG`
   helm init 
   helm repo add ibm-charts https://raw.githubusercontent.com/IBM/charts/master/repo/stable
